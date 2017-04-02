cbef7815901edb35380ba922f68c11d8f886df3c
Gabriel Bustamante <gabrielbusta@gmail.com>
2017-04-02 18:03:58 -0500

    omw

---
+++ test.py
@@ -0,0 +1,30 @@
+import cv2
+import socket
+from time import sleep
+from settings import *
+
+
+def main():
+    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote.connect((ROBOT_IP, REMOTE_CONTROL_PORT))
+
+    distance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    distance.connect((ROBOT_IP, ULTRASONIC_SENSOR_PORT))
+
+    cv2.namedWindow(ROBOT_IP, cv2.WINDOW_AUTOSIZE)
+    video = cv2.VideoCapture('tcp://{}:{}'.format(ROBOT_IP, CAMERA_PORT))
+    try:
+        streaming, frame = video.read()
+        while streaming:
+            cv2.imshow(ROBOT_IP, frame)
+            streaming, frame = video.read()
+    finally:
+        video.release()
+        remote.shutdown(socket.SHUT_RDWR)
+        remote.close()
+        distance.shutdown(socket.SHUT_RDWR)
+        distance.close()
+
+
+if __name__ == "__main__":
+    main()

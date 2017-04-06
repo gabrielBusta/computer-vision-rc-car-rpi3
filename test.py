d44fa584db19d7359d5771463175ae04dfc394db
Gabriel Bustamante <gabrielbusta@gmail.com>
2017-04-06 17:57:07 -0500

    deep learning exp

--- test.py
+++ test.py
@@ -25,8 +25,6 @@ import sys
 import numpy as np
 from multiprocessing import Process
 from settings import *
-from math import floor
-from utils import detectSquares
 
 
 def main():
@@ -62,15 +60,27 @@ def ultrasonic_sense():
         connection.close()
 
 
+def label(frame):
+    pass
+
+
 def vision():
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     video = cv2.VideoCapture('tcp://{}:{}'.format(ROBOT_IP, CAMERA_PORT))
     cv2.namedWindow(ROBOT_IP, cv2.WINDOW_NORMAL)
     try:
         streaming, frame = video.read()
         while streaming:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            squares = detectSquares(gray)
-            cv2.drawContours(frame, squares, -1, (0, 255, 0), 3)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
             drawbanner(frame)
             cv2.imshow(ROBOT_IP, frame)
             cv2.waitKey(1)
